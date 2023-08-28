60f52402c0db0a5e203db22902d5d4a5ff87f818
vboxuser <vboxuser@vboxuser.myguest.virtualbox.org>
2023-08-28 01:17:20 +0000

    themes and icons working

--- pretty.sh
+++ pretty.sh
@@ -3,14 +3,12 @@ mkdir $HOME/.icons
 
 unzip Gruvbox-Dark-B.zip
 tar -zxvf Gruvbox.tar.gz
+
 mv Gruvbox-Dark-B $HOME/.themes/
 mv Gruvbox $HOME/.icons/
 
-
 xfconf-query -c xsettings -p /Net/ThemeName -s "Gruvbox-Dark-B"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "Gruvboxi"
-
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Gruvbox"
 xfconf-query -c xfwm4 -p "/general/theme" -s "Gruvbox-Dark-B"
-xfwm4 --replace
 
 
