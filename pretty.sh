e76e4a928f96037d43b6ff2206e9018fac107af7
Sauce <telmobelasauce@ua.pt>
2023-09-06 10:16:00 +0000

    icons removal working

--- pretty.sh
+++ pretty.sh
@@ -1,3 +1,4 @@
+#Get themes
 mkdir $HOME/.themes
 mkdir $HOME/.icons
 
@@ -11,6 +12,7 @@ xfconf-query -c xsettings -p /Net/ThemeName -s "Gruvbox-Dark-B"
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Gruvbox"
 xfconf-query -c xfwm4 -p "/general/theme" -s "Gruvbox-Dark-B"
 
+#Panel
 sudo mv gtk.css $HOME/.config/gtk-3.0/gtk.css
 
 sudo mv xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
@@ -18,9 +20,11 @@ sudo mv xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-pan
 xfconf-query -c xfce4-panel -p "/panels/panel-1/background-style" -s "2"
 xfconf-query -c xfce4-panel -p "/panels/panel-1/background-image" -s "$HOME/Setup/Wallpapers/wallpaper2.png"
 
+#Background Image
 xfconf-query -c xfce4-desktop -p "/backdrop/screen0/monitorVirtual1/workspace0/last-image" -s "$HOME/Setup/Wallpapers/wallpaper1.jpg"
 
-xfconf-query -c xfce4-desktop -p /desktop-icons/style -s 0
+#remove icons Desktop
+xfconf-query -c xfce4-desktop -p /desktop-icons/style -s 0 --create
 
 xfce4-panel -r
 sudo reboot
