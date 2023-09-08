eeedee478801e2946a7565cf028b02dcea737e27
Sauce <telmobelasauce@ua.pt>
2023-09-08 14:43:28 +0000

    Adding versions validation
    I addded a validation using versions so if the cmd returns error the package is not installed

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+check_status() {
+    if [ $? -eq 0 ]; then
+        echo -e "\033[32m√ $1 installed successfully\033[0m"
+    else
+        echo -e "\033[31m✗ $1 installation failed\033[0m"
+    fi
+}
+
+
+verification=()
+ 
 #Upgrade System
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
 #VScode
 sudo snap install --classic code
+verification+=("code --version")
 
 #Tmux
 sudo apt-get install -y  tmux
+verification+=("tmux -V")
 
 #setup Tmux conf file
 rm $HOME/.tmux.conf 
@@ -42,19 +55,36 @@ bind -n M-Up    select-pane -U
 bind -n M-Down  select-pane -D
 EOT
 
+#Curl
+sudo apt install curl
+verification+=("curl -V")
+
 #Node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
-nvm install node
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
+verification+=("node --version")
+verification+=("npm -v")
 
 #Git
 sudo apt-get install -y git
+verification+=("git --version")
 
 #Create ssh Key
 ssh-keygen -t rsa -b 4096 -C "telmobelasauce@ua.pt"
 
 #enter this output into git ssh
 cat $HOME/.ssh/id_rsa.pub
+
+for item in "${verification[@]}"; do
+    $item &>/dev/null
+    check_status "$item"
+done
 
 read -p "Do you want to continue? (y/n): " choice
 
