c9d97f4d960754de661bc605a2b7784627a75b65
Sauce <telmobelasauce@ua.pt>
2023-09-16 10:25:28 +0000

    wallpaper not working fixed

--- themeInstalation.sh
+++ themeInstalation.sh
@@ -22,10 +22,10 @@ sudo mv gtk.css $HOME/.config/gtk-3.0/gtk.css
 sudo mv xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 
 xfconf-query -c xfce4-panel -p "/panels/panel-1/background-style" -s "2"
-xfconf-query -c xfce4-panel -p "/panels/panel-1/background-image" -s "$HOME/Setup/Wallpapers/wallpaper2.png"
+xfconf-query -c xfce4-panel -p "/panels/panel-1/background-image" -s "$HOME/Setup/Theme/Wallpapers/wallpaper2.png"
 
 #Background Image
-xfconf-query -c xfce4-desktop -p "/backdrop/screen0/monitorVirtual1/workspace0/last-image" -s "$HOME/Setup/Wallpapers/wallpaper1.jpg"
+xfconf-query -c xfce4-desktop -p "/backdrop/screen0/monitorVirtual1/workspace0/last-image" -s "$HOME/Setup/Theme/Wallpapers/wallpaper1.jpg"
 
 #remove icons Desktop
 xfconf-query -c xfce4-desktop -p /desktop-icons/style -s 0 -t int --create
